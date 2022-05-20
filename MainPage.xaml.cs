4377880ca5a6723da8f7fe31378d9f0929e9b407
James Montemagno <james.montemagno@gmail.com>
2022-05-20 14:19:14 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace RangeCollectionDemo;
+
+public partial class MainPage : ContentPage
+{
+    Random random = new Random();
+	public ObservableRangeCollection<string> Items { get; } = new ();
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = this;
+	}
+
+    private void CounterBtn_Clicked(object sender, EventArgs e)
+    {
+        var items = new[] { $"Item {random.Next(1, 100)}", $"Item {random.Next(1, 100)}", $"Item {random.Next(1, 100)}" };
+        Items.AddRange(items);
+    }
+
+    private void CounterBtnClear_Clicked(object sender, EventArgs e)
+    {
+        Items.Clear();
+    }
+}
+
